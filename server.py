591e4a4d9380f7404b0970c47a3960948c6bc35a
GiladEP <60490672+GiladEP@users.noreply.github.com>
2021-02-15 20:57:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -28,7 +28,3 @@ def update_data():
                             MM_temp=rooms["MM"]["temp"],
                             MM_timestamp=rooms["MM"]["timestamp"]
                             )
-
-if __name__ == '__main__':
-    # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
