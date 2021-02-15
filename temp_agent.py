577579d75c8315050aec8d5d74c9431effee3735
GiladEP <60490672+GiladEP@users.noreply.github.com>
2021-02-15 20:44:21 +0200

    Delete temp_agent.py

--- temp_agent.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-r = requests.post('http://127.0.0.1:5000/', json={"computer_room" : "Labs",
-                                                    "temp" : "2888",
-                                                    "timestamp" : "IT WORKED!!!",
-                                                 })
-r.status_code
