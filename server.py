3378560796c5752b7658024f3ace9af0272d1df7
GiladEP <60490672+GiladEP@users.noreply.github.com>
2021-02-23 14:09:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,15 +5,8 @@ import requests
 app = Flask(__name__)
 
 TEMP_THRESHOLD = "25"
-"""TELEGRAM_BOT_TOKEN = '1114209977:AAFJg8EbEXg43DRPAC6xg8_mf07sxu2ks8Q'
-CHAT_ID = "gimelm"""
 
 rooms = {"MM": {"temp": "", "timestamp": "", "color": ""}, "Labs": {"temp": "", "timestamp": "", "color": ""}}
-
-"""#notifies telegram on high temps in computer rooms
-def notify_telegram(computer_room, temp):
-    requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendmessage?chat_id=@" + CHAT_ID + "&text=Alert! Temperature is too high:\r\n " + computer_room + ": " + temp)
-    #print("HIGH TEMP in " + computer_room + ": " + high_temps)"""
 
 @app.route('/', methods=['GET', 'POST'])
 def update_data():
@@ -23,14 +16,12 @@ def update_data():
         
         rooms[request_data['computer_room']]["temp"] = request_data['temp']
         rooms[request_data['computer_room']]["timestamp"] = request_data['timestamp']
-        #rooms[request_data['computer_room']]["color"] = request_data['color']
 
         for computer_room in rooms:
             if rooms[computer_room]["temp"] < TEMP_THRESHOLD:
                 rooms[computer_room]["color"] = "bg-success"
             else:
                rooms[computer_room]["color"] = "bg-danger"
-               #notify_telegram(computer_room, rooms[computer_room]["temp"])  
 
         print(rooms)
 
