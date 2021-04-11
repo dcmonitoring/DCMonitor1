d9cc1377bdc5365fc499975dfa1bce06b1be5351
dcmonitoring <81240652+dcmonitoring@users.noreply.github.com>
2021-04-11 11:44:29 +0300

    Update server.py

--- server.py
+++ server.py
@@ -49,6 +49,6 @@ def update_data():
                             Gimel_temp1=rooms["Gimel"]["temp"][0],
                             Gimel_temp2=rooms["Gimel"]["temp"][1],
                             Gimel_temp3=rooms["Gimel"]["temp"][2],
-                            Gimel_timestamp=rooms["MM"]["timestamp"],
-                            Gimel_color=rooms["MM"]["color"]
+                            Gimel_timestamp=rooms["Gimel"]["timestamp"],
+                            Gimel_color=rooms["Gimel"]["color"]
                             )                 
