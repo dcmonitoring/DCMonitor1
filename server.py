c2688572a66bcc7f596128cc9cd077440666ee6c
dcmonitoring <81240652+dcmonitoring@users.noreply.github.com>
2021-04-28 16:54:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -39,9 +39,10 @@ def update_data():
         computer_room = request_data['computer_room']
         if computer_room not in rooms.keys():
             rooms[computer_room] = Room()
+        rooms[computer_room].timestamp = "check-me"
         rooms[computer_room].name = computer_room
         rooms[computer_room].temps = [int(te) for te in request_data['temp']]
-        rooms[computer_room].timestamp = "chek-me"
+
         rooms[computer_room].timestamp = request_data#['timestamp']
 
         if any(temp > TEMP_THRESHOLD for temp in rooms[computer_room].temps):
