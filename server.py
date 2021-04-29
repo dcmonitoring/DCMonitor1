50229b0a02cea613f0d271d8cf2bcf67379d5d57
dcmonitoring <81240652+dcmonitoring@users.noreply.github.com>
2021-04-29 14:27:16 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # import main Flask class and request object
 from flask import Flask, request, render_template
 import requests
+from datetime import datetime, timedelta
 # create the Flask app
 app = Flask(__name__)
 
@@ -8,28 +9,34 @@ TEMP_THRESHOLD = 25
 TELEGRAM_BOT_TOKEN = '1114209977:AAFJg8EbEXg43DRPAC6xg8_mf07sxu2ks8Q'
 CHAT_ID = "gimelm"
 
+
 class Room:
-    def __init__(self,Name="undefined",Temps=[],Timestamp="undefined",Color="unknown"):
-        self.name=Name
+    def __init__(self, Name="undefined", Temps=[], Timestamp="undefined", Color="unknown", time_message="ok"):
+        self.name = Name
         self.temps = Temps
         self.timestamp = Timestamp
         self.color = Color
+        self.time_message = time_message
+
 
 rooms = {}
 
-#rooms = {
-#    "Gimel": {
-#        "temp": [], "timestamp": "", "color": ""}, "Labs": {"temp": ["", ""], "timestamp": "", "color": ""}, "Messer": {"temp": ["", ""], "timestamp": "", "color": ""}}
 
-#notifies telegram on high temps in computer rooms
-def notify_telegram(computer_room, temps):
+def notify_telegram_temps(computer_room, temps):
     temp_str = ""
     i = 1
     for temp in temps:
         temp_str += "Sensor" + str(i) + ": " + str(temp) + "\r\n"
         i += 1
 
-    requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendmessage?chat_id=@" + CHAT_ID + "&text=Alert! Temperature in " + computer_room + " is too high:\r\n " + temp_str)
+    requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendmessage?chat_id=@" +
+                 CHAT_ID + "&text=Alert! Temperature in " + computer_room + " is too high:\r\n " + temp_str)
+
+
+def notify_telegram_update(computer_room, timestamp):
+    requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendmessage?chat_id=@" + CHAT_ID +
+                 "&text=Alert! The agent in" + computer_room + " didn`t send any message in more then 10 minutes.\r\n Last update in" + timestamp)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def update_data():
@@ -39,23 +46,38 @@ def update_data():
         computer_room = request_data['computer_room']
         if computer_room not in rooms.keys():
             rooms[computer_room] = Room()
-        
         rooms[computer_room].name = computer_room
-        rooms[computer_room].timestamp = "check-me"
-        rooms[computer_room].temps = [float(te) for te in request_data['temp']]
-
-        rooms[computer_room].timestamp = request_data['timestamp']
-
+        rooms[computer_room].temps = [int(te) for te in request_data['temp']]
+        rooms[computer_room].timestamp = request_data["timestamp"]
         if any(temp > TEMP_THRESHOLD for temp in rooms[computer_room].temps):
             rooms[computer_room].color = "red"
-            notify_telegram(computer_room, rooms[computer_room].temps)
+            notify_telegram_temps(computer_room, rooms[computer_room].temps)
         else:
             rooms[computer_room].color = "green"
+
+        try:
+            room_last_update = datetime.strptime(
+                request_data['timestamp'], "%a %b %d %H:%M:%S %Y")
+            time_now = datetime.now()
+            time_delta = time_now - room_last_update
+            if time_delta > timedelta(seconds=600):
+                rooms[computer_room].color = "red"
+                rooms[computer_room].time_message = "{h} Hours, {m} Minutes and {s} Seconds".format(h=str(time_delta).split(":")[0], m=str(
+                    time_delta).split(":")[1], s=str(time_delta).split(":")[2].split(".")[0])
+                notify_telegram_update(
+                    computer_room, rooms[computer_room].timestamp)
+            else:
+                rooms[computer_room].time_message = "ok"
+        except Exception as excepts:
+            rooms[computer_room].color = "red"
+            print(excepts)
+            if rooms[computer_room].time_message == "ok":
+                rooms[computer_room].time_message = "can't compare timestamps"
 
         print(rooms)
 
         return "200 - OK"
-    
+
     else:
         thresholds = {
             "TEMP_COLD_THRESHOLD": 14,
@@ -63,24 +85,8 @@ def update_data():
             "TEMP_MID_HOT_THRESHOLD": 25,
             "TEMP_HOT_THRESHOLD": 28,
         }
-        return render_template("chiller2.html", rooms=rooms,ts=thresholds)
-        """
-        return render_template('chiller.html',
-                            Labs_temp1=rooms["Labs"]["temp"][0],
-                            Labs_temp2=rooms["Labs"]["temp"][1],
-                            Labs_timestamp=rooms["Labs"]["timestamp"],
-                            Labs_color=rooms["Labs"]["color"],
-                            Messer_temp1=rooms["Messer"]["temp"][0],
-                            Messer_temp2=rooms["Messer"]["temp"][1],
-                            Messer_timestamp=rooms["Messer"]["timestamp"],
-                            Messer_color=rooms["Messer"]["color"],
-                            Gimel_temp1=rooms["Gimel"]["temp"][0],
-                            Gimel_temp2=rooms["Gimel"]["temp"][1],
-                            Gimel_temp3=rooms["Gimel"]["temp"][2],
-                            Gimel_timestamp=rooms["Gimel"]["timestamp"],
-                            Gimel_color=rooms["Gimel"]["color"]
-                            )
-                            """
-       
+        return render_template("chiller2.html", rooms=rooms, ts=thresholds)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
